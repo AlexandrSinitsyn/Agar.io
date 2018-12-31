be56271a1ae7c8441f6f254b8c3fc708aeb7164f
Sannitsa <39600558+Sannitsa@users.noreply.github.com>
2018-12-31 17:30:24 +0300

    Update Play.java

--- src/agar/io/Play.java
+++ src/agar/io/Play.java
@@ -8,20 +8,25 @@ import java.io.IOException;
 public class Play {
 
     public static void main(String[] args) throws IOException {
+        //Creating field to play
         new Field();
 
-        //Player player1 = new Player(567, Color.yellow);
-        //player1.Keyboard();
-        Player player2 = new Player(528, "test.jpg");
+        //To use you Player, you need to switch on Keyboard or Mouse.
+        Player player1 = new Player(567, Color.yellow);
+        player1.Keyboard();
+        Player player2 = new Player(528, "someImage.jpg");
         player2.Mouse();
 
+        //Create some bots to compete with
         new Bot(871);
         new Bot(216);
         new Bot(145);
         new Bot(2356);
 
+        //TopList is for knownig everybodies mass and so on
         new TopList();
 
+        //start play
         EGEngine.i().startDrawingThread();
     }
 }
